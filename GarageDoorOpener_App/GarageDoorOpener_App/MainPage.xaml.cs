d2a9c61086d2ffe8d27adcfae7ca03c6434d496f
Haydon <hbaddock@hotmail.co.nz>
2020-09-15 21:40:45 +1000

    implements garage door opener for Android

--- GarageDoorOpener_App/GarageDoorOpener_App/MainPage.xaml.cs
+++ GarageDoorOpener_App/GarageDoorOpener_App/MainPage.xaml.cs
@@ -1,17 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using GarageDoorOpener_App.Utilities;
+using System;
 using Xamarin.Forms;
 
 namespace GarageDoorOpener_App
 {
     public partial class MainPage : ContentPage
     {
+        private GarageDoor _garageDoor;
+
         public MainPage()
         {
             InitializeComponent();
+
+            BindingContext = this;
+
+            _garageDoor = new GarageDoor();
+        }
+
+        private async void Open_Clicked(object sender, EventArgs e)
+        {
+            await _garageDoor.Open();
         }
     }
 }
---
+++ GarageDoorOpener_App/GarageDoorOpener_App/Utilities/GarageDoor.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace GarageDoorOpener_App.Utilities
+{
+    public class GarageDoor
+    {
+        private const string PiAddress = "http://192.168.1.100:2000/open-sesame";
+
+        private readonly HttpClient _client;
+
+        public GarageDoor()
+        {
+            _client = new HttpClient();
+        }
+
+        public async Task<bool> Open()
+        {
+            try
+            {
+                await _client.GetAsync(PiAddress);
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+    }
+}
